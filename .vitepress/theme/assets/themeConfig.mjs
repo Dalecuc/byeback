32631bdabaff0fbd956f053e89603131c32fd0f8
DaleChu <ringingtrue7@gmail.com>
2024-10-21 22:40:52 +0800

    init

---
+++ .vitepress/theme/assets/themeConfig.mjs
@@ -0,0 +1,377 @@
+// 主题配置
+export const themeConfig = {
+  // 站点信息
+  siteMeta: {
+    // 站点标题
+    title: "~ BYEBACK ~",
+    // 站点描述
+    description: "L'Internationale, sera le genre humain",
+    // 站点logo
+    logo: "/images/logo/logo.webp",
+    // 站点地址
+    site: "http://8.152.158.36/",
+    // 语言
+    lang: "zh-CN",
+    // 作者
+    author: {
+      name: "BYEBACKers",
+      cover: "/images/logo/logo.webp",
+      email: "2448926122@qq.com",
+      link: "/",
+    },
+  },
+  // 备案信息
+  // icp: "萌ICP备114514号",
+  // 建站日期
+  since: "2024-10-21",
+  // 每页文章数据
+  postSize: 8,
+  // inject
+  inject: {
+    // 头部
+    // https://vitepress.dev/zh/reference/site-config#head
+    header: [
+      // favicon
+      ["link", { rel: "icon", href: "/favicon.ico" }],
+      // RSS
+      [
+        "link",
+        {
+          rel: "alternate",
+          type: "application/rss+xml",
+          title: "RSS",
+          href: "https://www.8.152.158.36/rss/feed.xml",
+        },
+      ],
+      // 预载 CDN
+      [
+        "link",
+        {
+          crossorigin: "",
+          rel: "preconnect",
+          href: "https://s1.hdslb.com",
+        },
+      ],
+      [
+        "link",
+        {
+          crossorigin: "",
+          rel: "preconnect",
+          href: "https://mirrors.sustech.edu.cn",
+        },
+      ],
+      // HarmonyOS font
+      [
+        "link",
+        {
+          crossorigin: "anonymous",
+          rel: "stylesheet",
+          href: "https://s1.hdslb.com/bfs/static/jinkela/long/font/regular.css",
+        },
+      ],
+      [
+        "link",
+        {
+          crossorigin: "anonymous",
+          rel: "stylesheet",
+          href: "https://mirrors.sustech.edu.cn/cdnjs/ajax/libs/lxgw-wenkai-screen-webfont/1.7.0/style.css",
+        },
+      ],
+      // iconfont
+      [
+        "link",
+        {
+          crossorigin: "anonymous",
+          rel: "stylesheet",
+          href: "https://cdn2.codesign.qq.com/icons/g5ZpEgx3z4VO6j2/latest/iconfont.css",
+        },
+      ],
+      // Embed code
+      ["link", { rel: "preconnect", href: "https://use.sevencdn.com" }],
+      ["link", { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" }],
+      [
+        "link",
+        {
+          crossorigin: "anonymous",
+          href: "https://use.sevencdn.com/css2?family=Fira+Code:wght@300..700&display=swap",
+          rel: "stylesheet",
+        },
+      ],
+      // 预载 DocSearch
+      [
+        "link",
+        {
+          href: "https://X5EBEZB53I-dsn.algolia.net",
+          rel: "preconnect",
+          crossorigin: "",
+        },
+      ],
+    ],
+  },
+  // 导航栏菜单
+  nav: [
+    {
+      text: "首页",
+      items: [
+        { text: "文章列表", link: "/", icon: "article" },
+      ],
+    },
+    {
+      text: "归档",
+      items: [
+        { text: "文章列表", link: "/pages/archives", icon: "article" },
+      ],
+    },
+    {
+       text: "零碎",
+       items: [
+          { text: "技术分享", link: "/pages/lifedots", icon: "technical" },
+       ],
+     },
+    {
+      text: "友链",
+      items: [
+        { text: "友链鱼塘", link: "/pages/friends", icon: "fish" },
+        // { text: "友情链接", link: "/pages/link", icon: "people" },
+      ],
+    },
+    {
+      text: "关于",
+      items: [
+        // { text: "畅所欲言", link: "/pages/message", icon: "chat" },
+        // { text: "致谢名单", link: "/pages/thanks", icon: "reward" },
+        { text: "关于本站", link: "/pages/about", icon: "contacts" },
+      ],
+    },
+    
+  ],
+
+
+  // 导航栏菜单 - 左侧
+  navMore: [
+    {
+      name: "博客",
+      list: [
+        {
+          icon: "/images/logo/logo.webp",
+          name: "主站",
+          url: "https://www.8.152.158.36/",
+        },
+        {
+          icon: "/images/logo/logo.webp",
+          name: "博客镜像站",
+          url: "https://blog-backup.imsyy.top/",
+        },
+      ],
+    },
+    {
+      name: "服务",
+      list: [
+        {
+          icon: "https://pic.imgdb.cn/item/653a8388c458853aefefcc58.png",
+          name: "站点监测",
+          url: "https://status.imsyy.top/",
+        },
+      ],
+    },
+    {
+      name: "项目",
+      list: [
+        {
+          icon: "/images/logo/logo.webp",
+          name: "Curve",
+          url: "https://github.com/imsyy/vitepress-theme-curve",
+        }
+      ],
+    },
+  ],
+  // 页脚信息
+  footer: {
+    // 社交链接（请确保为偶数个）
+    social: [
+      {
+        icon: "email",
+        link: "mailto:2448926122@qq.com",
+      },
+      {
+        icon: "github",
+        link: "https://www.github.com/Daleveral/",
+      },
+      // {
+      //   icon: "telegram",
+      //   link: "https://t.me/bottom_user",
+      // },
+      {
+        icon: "bilibili",
+        link: "https://space.bilibili.com/496029253",
+      },
+      {
+        icon: "qq",
+        link: "https://res.abeim.cn/api/qq/?qq=2448926122",
+      },
+      // {
+      //   icon: "twitter-x",
+      //   link: "https://twitter.com/iimmsyy",
+      // },
+    ],
+    // sitemap
+    sitemap: [
+      {
+        text: "博客",
+        items: [
+          // { text: "近期文章", link: "/" },
+          { text: "全部分类", link: "/pages/categories" },
+          { text: "全部标签", link: "/pages/tags" },
+          // { text: "文章归档", link: "/pages/archives", newTab: true },
+        ],
+      },
+      // {
+      //   text: "项目",
+      //   items: [
+      //     { text: "Home", link: "https://github.com/imsyy/home/", newTab: true },
+      //     { text: "SPlayer", link: "https://github.com/imsyy/SPlayer/", newTab: true },
+      //     { text: "DailyHotApi", link: "https://github.com/imsyy/DailyHotApi/", newTab: true },
+      //     { text: "Snavigation", link: "https://github.com/imsyy/Snavigation/", newTab: true },
+      //   ],
+      // },
+      // {
+      //   text: "专栏",
+      //   items: [
+      //     { text: "技术分享", link: "/pages/categories/技术分享" },
+      //     { text: "我的项目", link: "/pages/project" },
+      //     { text: "效率工具", link: "/pages/tools" },
+      //   ],
+      // },
+      {
+        text: "页面",
+        items: [
+          { text: "畅所欲言", link: "/pages/message" },
+          { text: "关于本站", link: "/pages/about" },
+          // { text: "隐私政策", link: "/pages/privacy" },
+          // { text: "版权协议", link: "/pages/cc" },
+        ],
+      },
+      {
+        text: "服务",
+        items: [
+          // { text: "站点状态", link: "https://status.imsyy.top/", newTab: true },
+          { text: "一个导航", link: "https://nav.imsyy.top/", newTab: true },
+          { text: "站点订阅", link: "https://8.152.158.36/rss.xml", newTab: true },
+          // {
+          //   text: "反馈投诉",
+          //   link: "https://eqnxweimkr5.feishu.cn/share/base/form/shrcnCXCPmxCKKJYI3RKUfefJre",
+          //   newTab: true,
+          // },
+        ],
+      },
+    ],
+  },
+  // 评论
+  comment: {
+    enable: true,
+    // 评论系统选择
+    type: "artalk",
+    // 评论系统配置
+    artalk: {
+      site: "",
+      server: "",
+    },
+  },
+  // 侧边栏
+  aside: {
+    // 站点简介
+    hello: {
+      enable: true,
+    },
+    // 目录
+    toc: {
+      enable: true,
+    },
+    // 标签
+    tags: {
+      enable: true,
+    },
+    // 倒计时
+    countDown: {
+      enable: true,
+      // 倒计时日期
+      data: {
+        name: "春节",
+        date: "2025-01-29",
+      },
+    },
+    // 站点数据
+    siteData: {
+      enable: true,
+    },
+  },
+  // 友链
+  friends: {
+    // 友链朋友圈
+    circleOfFriends: "",
+    // 动态友链
+    dynamicLink: {
+      server: "",
+      app_token: "",
+      table_id: "",
+    },
+  },
+  // 音乐播放器
+  music: {
+    enable: false,
+    // id
+    id: 9379831714,
+    // netease / tencent / kugou
+    server: "netease",
+    // playlist / album / song
+    type: "playlist",
+  },
+  // 搜索
+  // https://www.algolia.com/
+  search: {
+    enable: false,
+    appId: "",
+    apiKey: "",
+  },
+  // 打赏
+  rewardData: {
+    enable: true,
+    // 微信二维码
+    wechat: "https://pic.imgdb.cn/item/653a8388c458853aefefcc58.png",
+    // 支付宝二维码
+    alipay: "https://pic.imgdb.cn/item/653a8388c458853aefefcc58.png",
+  },
+  // 图片灯箱
+  fancybox: {
+    enable: true,
+    js: "https://mirrors.sustech.edu.cn/cdnjs/ajax/libs/fancyapps-ui/5.0.36/fancybox/fancybox.umd.min.js",
+    css: "https://mirrors.sustech.edu.cn/cdnjs/ajax/libs/fancyapps-ui/5.0.36/fancybox/fancybox.min.css",
+  },
+  // 外链中转
+  jumpRedirect: {
+    enable: true,
+    // 排除类名
+    exclude: [
+      "cf-friends-link",
+      "upyun",
+      "icp",
+      "author",
+      "rss",
+      "cc",
+      "power",
+      "social-link",
+      "link-text",
+      "travellings",
+      "post-link",
+      "report",
+      "more-link",
+      "skills-item",
+      "right-menu-link",
+      "link-card",
+    ],
+  },
+  // 站点统计
+  tongji: {
+    "51la": "",
+  },
+};
---
+++ .vitepress/theme/views/About.vue
@@ -0,0 +1,360 @@
+<template>
+  <div class="about">
+    <h1 class="title">关于本站</h1>
+
+    <!-- 心路历程 -->
+    <div class="about-content" style="display: flex">
+      <div class="about-item">
+        <!-- <span class="tip">心路历程</span> -->
+        <span class="title2">为什么建站？</span>
+        <p class="text">
+          创建这个站的时候，想要就是能够有一个自己能够<strong>积累知识</strong>、<strong>积累兴趣</strong>的地方。和他人分享，会让这些成为<strong>积累和沉淀</strong>。如果能够帮助到更多的人，帮助更多人解决问题，那一定是非常棒的事情。
+        </p>
+
+        <p class="text">
+          这些就是创造这个小站的本意，<strong>也是我分享生活的方式</strong>。有幸能和你相遇在这里，相信我们能共同留下一段美好记忆。
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { getStatistics } from "@/api";
+
+const { theme } = useData();
+
+// 站点统计数据
+const statisticsData = ref(null);
+
+// 获取站点统计数据
+const getStatisticsData = async () => {
+  const result = await getStatistics(theme.value.tongji["51la"]);
+  statisticsData.value = result;
+};
+
+onMounted(() => {
+  getStatisticsData();
+});
+</script>
+
+<style lang="scss" scoped>
+.about {
+  .title {
+    font-size: 2.4rem;
+    text-align: center;
+    border: none;
+  }
+  .about-content {
+    display: grid;
+    grid-template-columns: auto auto;
+    gap: 20px;
+    margin-bottom: 20px;
+    .about-item {
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      padding: 1.2rem 2rem;
+      border-radius: 12px;
+      background-color: var(--main-card-background);
+      border: 1px solid var(--main-card-border);
+      box-shadow: 0 8px 12px -4px var(--main-border-shadow);
+      overflow: hidden;
+      .tip {
+        font-size: 14px;
+        opacity: 0.8;
+        margin-bottom: 12px;
+      }
+      .title1 {
+        font-size: 36px;
+        font-weight: bold;
+        opacity: 0.6;
+      }
+      .title2 {
+        font-size: 36px;
+        font-weight: bold;
+        margin-right: 4rem;
+      }
+      .text {
+        font-size: 18px;
+        margin: 0.6rem 0;
+      }
+      &.child {
+        background-color: transparent;
+        border: none;
+        box-shadow: none;
+        padding: 0;
+        gap: 20px;
+        .about-item {
+          height: 100%;
+        }
+      }
+      &.hello {
+        justify-content: center;
+        padding: 2rem;
+        color: #fff;
+        background-image: linear-gradient(120deg, #5b27ff 0%, #00d4ff 100%);
+        background-size: 200% 200%;
+        animation: gradientFlow 6s ease infinite;
+        .title2 {
+          line-height: 2;
+        }
+      }
+      &.pursuit {
+        .title2 {
+          line-height: 1.2;
+          &:last-child {
+            display: inline-block;
+            background-size: 100% 100%;
+            background-clip: text;
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+            background-repeat: no-repeat;
+            background-image: linear-gradient(45deg, #fa7671 50%, #f45f7f);
+          }
+        }
+      }
+      &.character {
+        min-height: 220px;
+        cursor: pointer;
+        .more {
+          margin-top: auto;
+          font-size: 14px;
+          color: var(--main-color-gray);
+          a {
+            color: var(--main-color-gray);
+            &:hover {
+              color: var(--color);
+            }
+          }
+        }
+        .male {
+          position: absolute;
+          top: 20px;
+          right: -10px;
+          height: 140%;
+          width: auto;
+          transition: transform 0.5s;
+          transform-origin: top center;
+          @media (max-width: 768px) {
+            height: 80%;
+          }
+        }
+        &:hover {
+          .male {
+            transform: scale(1.2);
+          }
+        }
+      }
+      &.skills {
+        .skills-list {
+          margin-top: 12px;
+          display: flex;
+          flex-direction: row;
+          flex-wrap: wrap;
+          .skills-item {
+            display: flex;
+            align-items: center;
+            margin-right: 10px;
+            margin-top: 10px;
+            padding: 8px 12px 8px 8px;
+            border-radius: 40px;
+            background-color: var(--main-site-background);
+            border: 1px solid var(--main-card-border);
+            box-shadow: 0 8px 12px -4px var(--main-border-shadow);
+            transition: background-color 0.3s;
+            cursor: pointer;
+            .skills-logo {
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              width: 32px;
+              height: 32px;
+              margin-right: 8px;
+              border-radius: 50%;
+              background-color: var(--color);
+              .iconfont {
+                color: #fff;
+              }
+            }
+            .skills-name {
+              font-weight: bold;
+              transition: color 0.3s;
+            }
+            &:hover {
+              background-color: var(--main-card-background);
+            }
+          }
+        }
+      }
+      &.career {
+        .title2 {
+          letter-spacing: 0.2rem;
+          font-size: 40px;
+        }
+        .list {
+          margin-top: 12px;
+          display: flex;
+          flex-direction: column;
+          .list-item {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            margin-bottom: 12px;
+            color: var(--main-font-second-color);
+            &::before {
+              content: "";
+              display: block;
+              width: 16px;
+              height: 16px;
+              background-color: var(--color);
+              border-radius: 50%;
+              margin-right: 8px;
+            }
+          }
+        }
+        .career-img {
+          position: absolute;
+          bottom: -10px;
+          left: 0;
+          width: 100%;
+          @media (max-width: 768px) {
+            position: static;
+          }
+        }
+      }
+      &.game {
+        min-height: 300px;
+        @media (max-width: 768px) {
+          min-height: 240px;
+        }
+      }
+      &.like {
+        min-height: 400px;
+        @media (max-width: 768px) {
+          min-height: 300px;
+        }
+      }
+      &.image {
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+        .image-content {
+          flex-grow: 1;
+          display: flex;
+          flex-direction: column;
+          z-index: 2;
+          color: #fff;
+          .image-desc {
+            width: 100%;
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: space-between;
+            margin-top: auto;
+            &.opacity {
+              font-size: 14px;
+              color: #eee;
+              opacity: 0.8;
+              a {
+                color: #eee;
+                &:hover {
+                  color: var(--main-color);
+                }
+              }
+            }
+          }
+        }
+        &::after {
+          content: "";
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          top: 0;
+          left: 0;
+          box-shadow: inset 0 -70px 204px 10px var(--color);
+          z-index: 0;
+        }
+      }
+      &.static {
+        .static-data {
+          display: grid;
+          gap: 12px;
+          grid-template-columns: 1fr 1fr;
+          margin: 20px 0;
+          .static-item {
+            display: flex;
+            flex-direction: column;
+            .static-name {
+              font-size: 15px;
+              opacity: 0.8;
+            }
+            .static-num {
+              font-size: 34px;
+              font-weight: bold;
+            }
+          }
+        }
+      }
+      &.map {
+        min-height: 170px;
+        background-size: 100%;
+        transition: background 1.5s ease-in-out;
+        cursor: pointer;
+        @media (max-width: 768px) {
+          background-size: cover;
+          pointer-events: none;
+        }
+        .position {
+          display: block;
+          position: absolute;
+          left: 0;
+          bottom: 0;
+          width: 100%;
+          padding: 20px 30px;
+          color: #fff;
+          background-color: #636352;
+          font-size: 20px;
+          transition: bottom 1s;
+        }
+        &:hover {
+          background-size: 120%;
+          background-position-x: 0;
+          background-position-y: 36%;
+          .position {
+            bottom: -80px;
+          }
+        }
+      }
+      &.info {
+        flex-direction: row;
+        align-items: center;
+        justify-content: flex-start;
+        .info-item {
+          display: flex;
+          flex-direction: column;
+          margin-right: 32px;
+          .info-name {
+            font-size: 14px;
+            margin-bottom: 8px;
+            color: var(--main-font-second-color);
+          }
+          .info-num {
+            font-size: 34px;
+            font-weight: bold;
+            color: var(--color);
+          }
+        }
+      }
+    }
+    &:last-child {
+      margin-bottom: 0;
+    }
+    @media (max-width: 768px) {
+      display: flex;
+      flex-direction: column;
+    }
+  }
+}
+</style>
